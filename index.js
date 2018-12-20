a665435d308e78a4e01bc4c0eb3ef537aefaee8f
alejandro <alejandro@alejandros-MacBook-Air.local>
2018-12-19 20:13:30 -0800

    Endpoint returning json token implementation

--- index.js
+++ index.js
@@ -9,6 +9,27 @@ app.get('/api', (req, res) => {
     });
 });
 
+app.post('/api/newpost', (req, res) => {
+    res.json({
+        message: 'Post created...'
+    });
+});
+
+app.post('/api/login', (req, res) => {
+    //Mock user, in a real app you would go through auth and get a user back
+    const user = {
+        id: 1,
+        username: 'john doe',
+        email: 'john"email.com'
+    }
+
+    jwt.sign({user},'secretkey', (err, token) => {
+        res.json({
+            token
+        });
+    });
+})
+
 const PORT = 5000;
 
-app.listen(PORT, () => console.log(`Server started on port${PORT}`));
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
